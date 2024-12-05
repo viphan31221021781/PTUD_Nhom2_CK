b5be5ae57b82ed40a9b64101cafd508ef4828f7a
TgVy128 <thanhtuongvytran@gmail.com>
2024-12-05 20:17:33 +0700

    Vietsub admin\service

--- admin/service.php
+++ admin/service.php
@@ -2,10 +2,10 @@
 
 <section class="content-header">
 	<div class="content-header-left">
-		<h1>View Services</h1>
+		<h1>CÁC DỊCH VỤ</h1>
 	</div>
 	<div class="content-header-right">
-		<a href="service-add.php" class="btn btn-primary btn-sm">Add Service</a>
+		<a href="service-add.php" class="btn btn-primary btn-sm">Thêm</a>
 	</div>
 </section>
 
@@ -18,10 +18,10 @@
 						<thead>
 							<tr>
 								<th width="30">#</th>
-								<th>Photo</th>
-								<th width="100">Title</th>
-								<th>Content</th>
-								<th width="80">Action</th>
+								<th>Ảnh</th>
+								<th width="100">Tiêu Đề</th>
+								<th>Nội Dung</th>
+								<th width="80">Tùy Chọn</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -39,8 +39,8 @@
 									<td><?php echo $row['title']; ?></td>
 									<td><?php echo $row['content']; ?></td>
 									<td>										
-										<a href="service-edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary btn-xs">Edit</a>
-										<a href="#" class="btn btn-danger btn-xs" data-href="service-delete.php?id=<?php echo $row['id']; ?>" data-toggle="modal" data-target="#confirm-delete">Delete</a>  
+										<a href="service-edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary btn-xs">Cập nhật</a>
+										<a href="#" class="btn btn-danger btn-xs" data-href="service-delete.php?id=<?php echo $row['id']; ?>" data-toggle="modal" data-target="#confirm-delete">Xóa</a>  
 									</td>
 								</tr>
 								<?php
@@ -62,14 +62,14 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Delete Confirmation</h4>
+                <h4 class="modal-title" id="myModalLabel">XÁC NHẬN XÓA</h4>
             </div>
             <div class="modal-body">
-                <p>Are you sure want to delete this item?</p>
+                <p>Bạn có chắc chắn muốn xóa mục này không?</p>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-danger btn-ok">Delete</a>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Hủy bỏ</button>
+                <a class="btn btn-danger btn-ok">Xóa</a>
             </div>
         </div>
     </div>
