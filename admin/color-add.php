e4ec99facf5d6b7cc8c1cada377c651febcac487
nguyenphuc0121 <nguyenphuc0121@gmail.com>
2024-12-05 22:17:08 +0700

    Update color-add.php
    vietsub

--- admin/color-add.php
+++ admin/color-add.php
@@ -8,116 +8,86 @@ if(isset($_POST['form1'])) {
         $valid = 0;
         $error_message .= "Tên màu không được để trống<br>";
     } else {
-		// Kiểm tra trùng lặp tên màu
-    	// Lấy tên màu hiện tại trong cơ sở dữ liệu
-    	$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_id=?");
-		$statement->execute(array($_REQUEST['id']));
-		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-		foreach($result as $row) {
-			$current_color_name = $row['color_name'];
-		}
-
-		$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_name=? and color_name!=?");
-    	$statement->execute(array($_POST['color_name'],$current_color_name));
-    	$total = $statement->rowCount();							
-    	if($total) {
+    	// Kiểm tra trùng lặp
+    	$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_name=?");
+    	$statement->execute(array($_POST['color_name']));
+    	$total = $statement->rowCount();
+    	if($total)
+    	{
     		$valid = 0;
-        	$error_message .= 'Tên màu đã tồn tại<br>';
+        	$error_message .= "Tên màu đã tồn tại<br>";
     	}
     }
 
-    if($valid == 1) {    	
-		// Cập nhật vào cơ sở dữ liệu
-		$statement = $pdo->prepare("UPDATE tbl_color SET color_name=? WHERE color_id=?");
-		$statement->execute(array($_POST['color_name'],$_REQUEST['id']));
+    if($valid == 1) {
 
-    	$success_message = 'Cập nhật màu thành công.';
+		// Lưu dữ liệu vào bảng chính tbl_color
+		$statement = $pdo->prepare("INSERT INTO tbl_color (color_name) VALUES (?)");
+		$statement->execute(array($_POST['color_name']));
+	
+    	$success_message = 'Thêm màu thành công.';
     }
-}
-?>
-
-<?php
-if(!isset($_REQUEST['id'])) {
-	header('location: logout.php');
-	exit;
-} else {
-	// Kiểm tra ID có hợp lệ không
-	$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_id=?");
-	$statement->execute(array($_REQUEST['id']));
-	$total = $statement->rowCount();
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-	if( $total == 0 ) {
-		header('location: logout.php');
-		exit;
-	}
 }
 ?>
 
 <section class="content-header">
 	<div class="content-header-left">
-		<h1>Chỉnh Sửa Màu</h1>
+		<h1>Thêm Màu</h1>
 	</div>
 	<div class="content-header-right">
 		<a href="color.php" class="btn btn-primary btn-sm">Xem Tất Cả</a>
 	</div>
 </section>
 
-
-<?php							
-foreach ($result as $row) {
-	$color_name = $row['color_name'];
-}
-?>
-
 <section class="content">
 
-  <div class="row">
-    <div class="col-md-12">
+	<div class="row">
+		<div class="col-md-12">
 
-		<?php if($error_message): ?>
-		<div class="callout callout-danger">
-		
-		<p>
-		<?php echo $error_message; ?>
-		</p>
+			<?php if($error_message): ?>
+			<div class="callout callout-danger">
+			
+			<p>
+			<?php echo $error_message; ?>
+			</p>
+			</div>
+			<?php endif; ?>
+
+			<?php if($success_message): ?>
+			<div class="callout callout-success">
+			
+			<p><?php echo $success_message; ?></p>
+			</div>
+			<?php endif; ?>
+
+			<form class="form-horizontal" action="" method="post">
+
+				<div class="box box-info">
+					<div class="box-body">
+						<div class="form-group">
+							<label for="" class="col-sm-2 control-label">Tên Màu <span>*</span></label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" name="color_name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="" class="col-sm-2 control-label"></label>
+							<div class="col-sm-6">
+								<button type="submit" class="btn btn-success pull-left" name="form1">Gửi</button>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</form>
+
+
 		</div>
-		<?php endif; ?>
+	</div>
 
-		<?php if($success_message): ?>
-		<div class="callout callout-success">
-		
-		<p><?php echo $success_message; ?></p>
-		</div>
-		<?php endif; ?>
+</section>
 
-        <form class="form-horizontal" action="" method="post">
-
-        <div class="box box-info">
-
-            <div class="box-body">
-                <div class="form-group">
-                    <label for="" class="col-sm-2 control-label">Tên Màu <span>*</span></label>
-                    <div class="col-sm-4">
-                        <input type="text" class="form-control" name="color_name" value="<?php echo $color_name; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                	<label for="" class="col-sm-2 control-label"></label>
-                    <div class="col-sm-6">
-                      <button type="submit" class="btn btn-success pull-left" name="form1">Cập Nhật</button>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-        </form>
-
-
-
-    </div>
-  </div>
+<?php require_once('footer.php'); ?>
 
 </section>
 
