6bed28f40e5c484172b2126f45167f969b0b2c65
Qngueek <quynhvo.ueh@gmail.com>
2024-12-10 15:28:51 +0700

    color

--- admin/color.php
+++ admin/color.php
@@ -1,91 +1,72 @@
 <?php require_once('header.php'); ?>
 
-<?php
-if(isset($_POST['form1'])) {
-	$valid = 1;
-
-    if(empty($_POST['color_name'])) {
-        $valid = 0;
-        $error_message .= "Tên màu không được để trống<br>";
-    } else {
-    	// Kiểm tra trùng lặp
-    	$statement = $pdo->prepare("SELECT * FROM tbl_color WHERE color_name=?");
-    	$statement->execute(array($_POST['color_name']));
-    	$total = $statement->rowCount();
-    	if($total)
-    	{
-    		$valid = 0;
-        	$error_message .= "Tên màu đã tồn tại<br>";
-    	}
-    }
-
-    if($valid == 1) {
-
-		// Lưu dữ liệu vào bảng chính tbl_color
-		$statement = $pdo->prepare("INSERT INTO tbl_color (color_name) VALUES (?)");
-		$statement->execute(array($_POST['color_name']));
-	
-    	$success_message = 'Thêm màu thành công.';
-    }
-}
-?>
-
 <section class="content-header">
 	<div class="content-header-left">
-		<h1>Thêm Màu</h1>
+		<h1>Xem màu sắc</h1>
 	</div>
 	<div class="content-header-right">
-		<a href="color.php" class="btn btn-primary btn-sm">Xem Tất Cả</a>
+		<a href="color-add.php" class="btn btn-primary btn-sm">Thêm Mới</a>
 	</div>
 </section>
 
 <section class="content">
 
-	<div class="row">
-		<div class="col-md-12">
+  <div class="row">
+    <div class="col-md-12">
 
-			<?php if($error_message): ?>
-			<div class="callout callout-danger">
-			
-			<p>
-			<?php echo $error_message; ?>
-			</p>
-			</div>
-			<?php endif; ?>
-
-			<?php if($success_message): ?>
-			<div class="callout callout-success">
-			
-			<p><?php echo $success_message; ?></p>
-			</div>
-			<?php endif; ?>
-
-			<form class="form-horizontal" action="" method="post">
-
-				<div class="box box-info">
-					<div class="box-body">
-						<div class="form-group">
-							<label for="" class="col-sm-2 control-label">Tên Màu <span>*</span></label>
-							<div class="col-sm-4">
-								<input type="text" class="form-control" name="color_name">
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="" class="col-sm-2 control-label"></label>
-							<div class="col-sm-6">
-								<button type="submit" class="btn btn-success pull-left" name="form1">Gửi</button>
-							</div>
-						</div>
-					</div>
-				</div>
-
-			</form>
-
-
-		</div>
-	</div>
+      <div class="box box-info">
+        <div class="box-body table-responsive">
+          <table id="example1" class="table table-bordered table-hover table-striped">
+			<thead>
+			    <tr>
+			        <th>#</th>
+			        <th>Màu</th>
+			        <th>Hành Động</th>
+			    </tr>
+			</thead>
+            <tbody>
+            	<?php
+            	$i = 0;
+            	$statement = $pdo->prepare("SELECT * FROM tbl_color ORDER BY color_id ASC");
+            	$statement->execute();
+            	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
+            	foreach ($result as $row) {
+            		$i++;
+            		?>
+					<tr>
+	                    <td><?php echo $i; ?></td>
+	                    <td><?php echo $row['color_name']; ?></td>
+	                    <td>
+	                        <a href="color-edit.php?id=<?php echo $row['color_id']; ?>" class="btn btn-primary btn-xs">Chỉnh Sửa</a>
+	                        <a href="#" class="btn btn-danger btn-xs" data-href="color-delete.php?id=<?php echo $row['color_id']; ?>" data-toggle="modal" data-target="#confirm-delete">Xóa</a>
+	                    </td>
+	                </tr>
+            		<?php
+            	}
+            	?>
+            </tbody>
+          </table>
+        </div>
+      </div>
 
 </section>
 
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Xác Nhận Xóa</h4>
+            </div>
+            <div class="modal-body">
+                Bạn có chắc chắn muốn xóa mục này không?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Hủy</button>
+                <a class="btn btn-danger btn-ok">Xóa</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require_once('footer.php'); ?>
-//
