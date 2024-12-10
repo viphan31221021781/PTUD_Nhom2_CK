f2070c944031c65ca5286f1ad2418c08a56729c6
nguyenphuc0121 <nguyenphuc0121@gmail.com>
2024-12-10 17:10:43 +0700

    Fix lỗi số 7 ở trong docs localhost 8088

--- footer.php
+++ footer.php
@@ -4,29 +4,35 @@ $statement->execute();
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $row)
 {
-	$footer_about = $row['footer_about'];
-	$contact_email = $row['contact_email'];
-	$contact_phone = $row['contact_phone'];
-	$contact_address = $row['contact_address'];
-	$footer_copyright = $row['footer_copyright'];
-	$total_recent_post_footer = $row['total_recent_post_footer'];
+    $footer_about = $row['footer_about'];
+    $contact_email = $row['contact_email'];
+    $contact_phone = $row['contact_phone'];
+    $contact_address = $row['contact_address'];
+    $footer_copyright = $row['footer_copyright'];
+    $total_recent_post_footer = $row['total_recent_post_footer'];
     $total_popular_post_footer = $row['total_popular_post_footer'];
     $newsletter_on_off = $row['newsletter_on_off'];
     $before_body = $row['before_body'];
 }
 ?>
 <style>
-	.home-newsletter {
+    .home-newsletter {
     background-color: #f5f5f5; /* Thay đổi màu nền quanh phần đăng ký */
     padding: 30px 0; /* Thêm khoảng cách trên dưới */
 }
-	/* Khung bên trong */
-	.home-newsletter .container {
+    /* Khung bên trong */
+    .home-newsletter .container {
     background-color: #c18d8f;
     border-radius: 10px;
     padding: 20px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
+
+
+
+
+
+
 
 
 .home-newsletter .single {
@@ -36,6 +42,9 @@ foreach ($result as $row)
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Tạo bóng cho phần form */
 }
 
+
+
+
 /* Tiêu đề */
 .home-newsletter h2 {
     color: #000000 !important;
@@ -44,9 +53,15 @@ foreach ($result as $row)
     margin-bottom: 20px;
 }
 
+
+
+
 .home-newsletter .input-group input {
     border-radius: 4px; /* Bo góc cho ô nhập liệu */
 }
+
+
+
 
 .home-newsletter .btn-theme {
     background-color: #931926; /* Màu nền cho nút đăng ký */
@@ -62,6 +77,9 @@ foreach ($result as $row)
     padding: 10px 20px;
 }
 
+
+
+
 .home-newsletter .btn-theme:hover {
     background-color: #e67e22; /* Màu nền khi hover nút đăng ký */
 }
@@ -71,119 +89,155 @@ foreach ($result as $row)
     color: #000;
 }
 
+
+
+
 </style>
 <?php if($newsletter_on_off == 1): ?>
 <section class="home-newsletter">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<div class="single">
-					<?php
-			if(isset($_POST['form_subscribe']))
-			{
-				if(empty($_POST['email_subscribe'])) 
-			    {
-			        $valid = 0;
-			        $error_message1 .= LANG_VALUE_131;
-			    }
-			    else
-			    {
-			    	if (filter_var($_POST['email_subscribe'], FILTER_VALIDATE_EMAIL) === false)
-				    {
-				        $valid = 0;
-				        $error_message1 .= LANG_VALUE_134;
-				    }
-				    else
-				    {
-				    	$statement = $pdo->prepare("SELECT * FROM tbl_subscriber WHERE subs_email=?");
-				    	$statement->execute(array($_POST['email_subscribe']));
-				    	$total = $statement->rowCount();							
-				    	if($total)
-				    	{
-				    		$valid = 0;
-				        	$error_message1 .= LANG_VALUE_147;
-				    	}
-				    	else
-				    	{
-				    		// Sending email to the requested subscriber for email confirmation
-				    		// Getting activation key to send via email. also it will be saved to database until user click on the activation link.
-				    		$key = md5(uniqid(rand(), true));
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="single">
+                    <?php
+            if(isset($_POST['form_subscribe']))
+            {
+                if(empty($_POST['email_subscribe']))
+                {
+                    $valid = 0;
+                    $error_message1 .= "Email không được để trống";
+                }
+                else
+                {
+                    if (filter_var($_POST['email_subscribe'], FILTER_VALIDATE_EMAIL) === false)
+                    {
+                        $valid = 0;
+                        $error_message1 .= "Email không hợp lệ";
+                    }
+                    else
+                    {
+                        $statement = $pdo->prepare("SELECT * FROM tbl_subscriber WHERE subs_email=?");
+                        $statement->execute(array($_POST['email_subscribe']));
+                        $total = $statement->rowCount();                            
+                        if($total)
+                        {
+                            $valid = 0;
+                            $error_message1 .= "Email này đã được sử dụng, vui lòng nhập email mới";
+                        }
+                        else
+                        {
+                            // Sending email to the requested subscriber for email confirmation
+                            // Getting activation key to send via email. also it will be saved to database until user click on the activation link.
+                            $key = md5(uniqid(rand(), true));
 
-				    		// Getting current date
-				    		$current_date = date('Y-m-d');
 
-				    		// Getting current date and time
-				    		$current_date_time = date('Y-m-d H:i:s');
 
-				    		// Inserting data into the database
-				    		$statement = $pdo->prepare("INSERT INTO tbl_subscriber (subs_email,subs_date,subs_date_time,subs_hash,subs_active) VALUES (?,?,?,?,?)");
-				    		$statement->execute(array($_POST['email_subscribe'],$current_date,$current_date_time,$key,0));
 
-				    		// Sending Confirmation Email
-				    		$to = $_POST['email_subscribe'];
-							$subject = 'Subscriber Email Confirmation';
-							
-							// Getting the url of the verification link
-							$verification_url = BASE_URL.'verify.php?email='.$to.'&key='.$key;
+                            // Getting current date
+                            $current_date = date('Y-m-d');
 
-							$message = '
+
+
+
+                            // Getting current date and time
+                            $current_date_time = date('Y-m-d H:i:s');
+
+
+
+
+                            // Inserting data into the database
+                            $statement = $pdo->prepare("INSERT INTO tbl_subscriber (subs_email,subs_date,subs_date_time,subs_hash,subs_active) VALUES (?,?,?,?,?)");
+                            $statement->execute(array($_POST['email_subscribe'],$current_date,$current_date_time,$key,0));
+
+
+
+
+                            // Sending Confirmation Email
+                            $to = $_POST['email_subscribe'];
+                            $subject = 'Subscriber Email Confirmation';
+                           
+                            // Getting the url of the verification link
+                            $verification_url = BASE_URL.'verify.php?email='.$to.'&key='.$key;
+
+
+
+
+                            $message = '
 Chân thành cảm ơn bạn đã quan tâm và đăng ký nhận thông báo từ chúng tôi!<br><br>
 Vui lòng nhấp vào liên kết này để xác nhận đăng ký của bạn:
-					'.$verification_url.'<br><br>
+                    '.$verification_url.'<br><br>
 Liên kết này chỉ có hiệu lực trong 24 giờ.
-					';
+                    ';
 
-							$headers = 'From: ' . $contact_email . "\r\n" .
-								   'Reply-To: ' . $contact_email . "\r\n" .
-								   'X-Mailer: PHP/' . phpversion() . "\r\n" . 
-								   "MIME-Version: 1.0\r\n" . 
-								   "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-							// Sending the email
-							mail($to, $subject, $message, $headers);
 
-							$success_message1 = LANG_VALUE_136;
-				    	}
-				    }
-			    }
-			}
-			if($error_message1 != '') {
-				echo "<script>alert('".$error_message1."')</script>";
-			}
-			if($success_message1 != '') {
-				echo "<script>alert('".$success_message1."')</script>";
-			}
-			?>
-				<form action="" method="post">
-					<?php $csrf->echoInputField(); ?>
-					<h2>Đăng ký để nhận thông báo sớm nhất</h2>
-					<div class="input-group">
-			        	<input type="email" class="form-control" placeholder="<?php echo "Nhập Email của bạn vào"; ?>" name="email_subscribe">
-			         	<span class="input-group-btn">
-			         	<button class="btn btn-theme" type="submit" name="form_subscribe"><?php echo "Đăng ký"; ?></button>
-			         	</span>
-			        </div>
-				</div>
-				</form>
-			</div>
-		</div>
-	</div>
+
+                            $headers = 'From: ' . $contact_email . "\r\n" .
+                                   'Reply-To: ' . $contact_email . "\r\n" .
+                                   'X-Mailer: PHP/' . phpversion() . "\r\n" .
+                                   "MIME-Version: 1.0\r\n" .
+                                   "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+
+
+                            // Sending the email
+                            mail($to, $subject, $message, $headers);
+
+
+
+
+                            $success_message1 = "Bạn đã đăng ký thành công";
+                        }
+                    }
+                }
+            }
+            if($error_message1 != '') {
+                echo "<script>alert('".$error_message1."')</script>";
+            }
+            if($success_message1 != '') {
+                echo "<script>alert('".$success_message1."')</script>";
+            }
+            ?>
+                <form action="" method="post">
+                    <?php $csrf->echoInputField(); ?>
+                    <h2>Đăng ký để nhận thông báo sớm nhất</h2>
+                    <div class="input-group">
+                        <input type="email" class="form-control" placeholder="<?php echo "Nhập Email của bạn vào"; ?>" name="email_subscribe">
+                        <span class="input-group-btn">
+                        <button class="btn btn-theme" type="submit" name="form_subscribe"><?php echo "Đăng ký"; ?></button>
+                        </span>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
 <?php endif; ?>
 
+
+
+
 <!-- <div class="footer-bottom">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 copyright">
-				<?php echo $footer_copyright; ?>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 copyright">
+                <?php echo $footer_copyright; ?>
+            </div>
+        </div>
+    </div>
 </div> -->
 
+
+
+
 <!-- <a href="#" class="scrollup">
-	<i class="fa fa-angle-up"></i>
+    <i class="fa fa-angle-up"></i>
 </a> -->
+
+
+
 
 <?php
 $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
@@ -194,6 +248,9 @@ foreach ($result as $row) {
     $stripe_secret_key = $row['stripe_secret_key'];
 }
 ?>
+
+
+
 
 <script src="assets/js/jquery-2.2.4.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
@@ -209,40 +266,49 @@ foreach ($result as $row) {
 <script src="assets/js/select2.full.min.js"></script>
 <script src="assets/js/custom.js"></script>
 <script>
-	function confirmDelete()
-	{
-	    return confirm("Bạn có chắc chắn muốn xóa dữ liệu này không?");
-	}
-	$(document).ready(function () {
-		advFieldsStatus = $('#advFieldsStatus').val();
+    function confirmDelete()
+    {
+        return confirm("Bạn có chắc chắn muốn xóa dữ liệu này không?");
+    }
+    $(document).ready(function () {
+        advFieldsStatus = $('#advFieldsStatus').val();
 
-		$('#paypal_form').hide();
-		$('#stripe_form').hide();
-		$('#bank_form').hide();
+
+
+
+        $('#paypal_form').hide();
+        $('#stripe_form').hide();
+        $('#bank_form').hide();
+
+
+
 
         $('#advFieldsStatus').on('change',function() {
             advFieldsStatus = $('#advFieldsStatus').val();
             if ( advFieldsStatus == '' ) {
-            	$('#paypal_form').hide();
-				$('#stripe_form').hide();
-				$('#bank_form').hide();
+                $('#paypal_form').hide();
+                $('#stripe_form').hide();
+                $('#bank_form').hide();
             } else if ( advFieldsStatus == 'PayPal' ) {
-               	$('#paypal_form').show();
-				$('#stripe_form').hide();
-				$('#bank_form').hide();
+                $('#paypal_form').show();
+                $('#stripe_form').hide();
+                $('#bank_form').hide();
             } else if ( advFieldsStatus == 'Stripe' ) {
-               	$('#paypal_form').hide();
-				$('#stripe_form').show();
-				$('#bank_form').hide();
+                $('#paypal_form').hide();
+                $('#stripe_form').show();
+                $('#bank_form').hide();
             } else if ( advFieldsStatus == 'Bank Deposit' ) {
-            	$('#paypal_form').hide();
-				$('#stripe_form').hide();
-				$('#bank_form').show();
+                $('#paypal_form').hide();
+                $('#stripe_form').hide();
+                $('#bank_form').show();
             }
         });
-	});
+    });
 
-	$(document).on('submit', '#stripe_form', function () {
+
+
+
+    $(document).on('submit', '#stripe_form', function () {
         // createToken returns immediately - the supplied callback submits the form if there are no errors
         $('#submit-button').prop("disabled", true);
         $("#msg-container").hide();
@@ -272,3 +338,6 @@ foreach ($result as $row) {
 <?php echo $before_body; ?>
 </body>
 </html>
+
+
+
