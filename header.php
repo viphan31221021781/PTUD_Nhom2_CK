30154347cfbe2d24aeab2a9b35f425dd83519034
viphan31221021781 <viphan.31221021781@st.ueh.edu.vn>
2024-12-10 20:35:09 +0700

    Update header.php

--- header.php
+++ header.php
@@ -311,7 +311,7 @@ foreach ($result as $row) {
 					<?php
 					if(isset($_SESSION['customer'])) {
 						?>
-						<li><i class="fa fa-user"></i> <?php echo "LANG_VALUE_13"; ?> <?php echo $_SESSION['customer']['cust_name']; ?></li>
+						<li><i class="fa fa-user"></i> <?php echo $_SESSION['customer']['cust_name']; ?></li>
 						<li><a href="dashboard.php"><i class="fa fa-home"></i> <?php echo LANG_VALUE_89; ?></a></li>
 						<?php
 					} else {
