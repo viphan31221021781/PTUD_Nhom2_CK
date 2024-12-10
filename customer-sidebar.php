362c2b7d537d274e4d0a67b0b8d1245bc096aee0
viphan31221021781 <viphan.31221021781@st.ueh.edu.vn>
2024-12-10 22:22:05 +0700

    Update customer-sidebar.php

--- customer-sidebar.php
+++ customer-sidebar.php
@@ -1,10 +1,10 @@
 <div class="user-sidebar">
     <ul>
-    <a href="dashboard.php"><button class="btn btn-danger"><?php echo LANG_VALUE_89; ?></button></a>
-    <a href="customer-profile-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_117; ?></button></a>
-        <a href="customer-billing-shipping-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_88; ?></button></a>
-        <a href="customer-password-update.php"><button class="btn btn-danger"><?php echo LANG_VALUE_99; ?></button></a>
-        <a href="customer-order.php"><button class="btn btn-danger"><?php echo LANG_VALUE_24; ?></button></a>
-        <a href="logout.php"><button class="btn btn-danger"><?php echo LANG_VALUE_14; ?></button></a>
+    <a href="dashboard.php"><button class="btn btn-danger"><?php echo "Trang cá nhân"; ?></button></a>
+    <a href="customer-profile-update.php"><button class="btn btn-danger"><?php echo "Chỉnh sửa thông tin cá nhân"; ?></button></a>
+        <a href="customer-billing-shipping-update.php"><button class="btn btn-danger"><?php echo "Chỉnh sửa thông tin giao hàng"; ?></button></a>
+        <a href="customer-password-update.php"><button class="btn btn-danger"><?php echo "Chỉnh sửa mật khẩu"; ?></button></a>
+        <a href="customer-order.php"><button class="btn btn-danger"><?php echo "Đơn hàng"; ?></button></a>
+        <a href="logout.php"><button class="btn btn-danger"><?php echo "Đăng xuất"; ?></button></a>
     </ul>
 </div>
