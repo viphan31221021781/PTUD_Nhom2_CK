cc2f2f132040923fafe0329a3e491f06865c9ea2
TgVy128 <thanhtuongvytran@gmail.com>
2024-12-14 00:06:57 +0700

    freeship cho đơn trên 1tr500

--- admin/shipping-cost.php
+++ admin/shipping-cost.php
@@ -30,7 +30,6 @@ if(isset($_POST['form1'])) {
 
 }
 
-
 if(isset($_POST['form2'])) {
     $valid = 1;
 
@@ -49,12 +48,11 @@ if(isset($_POST['form2'])) {
         $statement = $pdo->prepare("UPDATE tbl_shipping_cost_all SET amount=? WHERE sca_id=1");
         $statement->execute(array($_POST['amount']));
 
-        $success_message = 'Phí vận chuyển cho cho khu vực còn lại được thêm thành công!';
+        $success_message = 'Phí vận chuyển đặc biệt được thêm thành công!';
 
     }
 }
 ?>
-
 
 <section class="content-header">
     <div class="content-header-left">
@@ -69,17 +67,15 @@ if(isset($_POST['form2'])) {
 
             <?php if($error_message): ?>
             <div class="callout callout-danger">
-            
-            <p>
-            <?php echo $error_message; ?>
-            </p>
+                <p>
+                <?php echo $error_message; ?>
+                </p>
             </div>
             <?php endif; ?>
 
             <?php if($success_message): ?>
             <div class="callout callout-success">
-            
-            <p><?php echo $success_message; ?></p>
+                <p><?php echo $success_message; ?></p>
             </div>
             <?php endif; ?>
 
@@ -97,7 +93,6 @@ if(isset($_POST['form2'])) {
                                     $statement->execute();
                                     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
                                     foreach ($result as $row) {
-
 
                                         $statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost WHERE country_id=?");
                                         $statement->execute(array($row['country_id']));
@@ -131,70 +126,99 @@ if(isset($_POST['form2'])) {
 
             </form>
 
-
         </div>
     </div>
 </section>
 
-
-
-
 <section class="content-header">
-	<div class="content-header-left">
-		<h1>XEM DANH SÁCH PHÍ VẬN CHUYỂN</h1>
-	</div>
+    <div class="content-header-left">
+        <h1>PHÍ VẬN CHUYỂN ĐẶC BIỆT</h1>
+    </div>
 </section>
 
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <form class="form-horizontal" action="" method="post">
+                <div class="box box-info">
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="" class="col-sm-2 control-label">Phí vận chuyển <span>*</span></label>
+                            <div class="col-sm-4">
+                                <?php
+                                $statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost_all WHERE sca_id=1");
+                                $statement->execute();
+                                $result = $statement->fetch(PDO::FETCH_ASSOC);
+                                $current_amount = $result['amount'];
+                                ?>
+                                <input type="text" class="form-control" name="amount" value="<?php echo $current_amount; ?>">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="col-sm-2 control-label"></label>
+                            <div class="col-sm-6">
+                                <button type="submit" class="btn btn-success pull-left" name="form2">Cập nhật</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<section class="content-header">
+    <div class="content-header-left">
+        <h1>XEM DANH SÁCH PHÍ VẬN CHUYỂN</h1>
+    </div>
+</section>
 
 <section class="content">
 
   <div class="row">
     <div class="col-md-12">
 
-
       <div class="box box-info">
         
         <div class="box-body table-responsive">
           <table id="example1" class="table table-bordered table-hover table-striped">
-			<thead>
-			    <tr>
-			        <th>STT</th>
-			        <th>Tỉnh thành</th>
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Tỉnh thành</th>
                     <th>Phí vận chuyển</th>
-			        <th>Tùy chọn</th>
-			    </tr>
-			</thead>
+                    <th>Tùy chọn</th>
+                </tr>
+            </thead>
             <tbody>
-            	<?php
-            	$i=0;
-            	$statement = $pdo->prepare("SELECT * 
+                <?php
+                $i=0;
+                $statement = $pdo->prepare("SELECT * 
                                         FROM tbl_shipping_cost t1
                                         JOIN tbl_country t2 
                                         ON t1.country_id = t2.country_id 
                                         ORDER BY t2.country_name ASC");
-            	$statement->execute();
-            	$result = $statement->fetchAll(PDO::FETCH_ASSOC);							
-            	foreach ($result as $row) {
-            		$i++;
-            		?>
-					<tr>
-	                    <td><?php echo $i; ?></td>
-	                    <td><?php echo $row['country_name']; ?></td>
+                $statement->execute();
+                $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+                foreach ($result as $row) {
+                    $i++;
+                    ?>
+                    <tr>
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $row['country_name']; ?></td>
                         <td><?php echo $row['amount']; ?></td>
-	                    <td>
-	                        <a href="shipping-cost-edit.php?id=<?php echo $row['shipping_cost_id']; ?>" class="btn btn-primary btn-xs">Cập nhật</a>
-	                        <a href="#" class="btn btn-danger btn-xs" data-href="shipping-cost-delete.php?id=<?php echo $row['shipping_cost_id']; ?>" data-toggle="modal" data-target="#confirm-delete">Xóa</a>
-	                    </td>
-	                </tr>
-            		<?php
-            	}
-            	?>
+                        <td>
+                            <a href="shipping-cost-edit.php?id=<?php echo $row['shipping_cost_id']; ?>" class="btn btn-primary btn-xs">Cập nhật</a>
+                            <a href="#" class="btn btn-danger btn-xs" data-href="shipping-cost-delete.php?id=<?php echo $row['shipping_cost_id']; ?>" data-toggle="modal" data-target="#confirm-delete">Xóa</a>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
             </tbody>
           </table>
         </div>
       </div> 
-
-      
 
 </section>
 
--- checkout.php
+++ checkout.php
@@ -1,14 +1,14 @@
 <?php require_once('header.php'); ?>
-
 <?php
 // Lấy thông tin banner từ cơ sở dữ liệu
 $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
 $statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);                             
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
 foreach ($result as $row) {
     $banner_checkout = $row['banner_checkout'];
 }
 ?>
+
 
 <?php
 // Kiểm tra giỏ hàng
@@ -18,23 +18,25 @@ if(!isset($_SESSION['cart_p_id'])) {
 }
 ?>
 
+
 <div class="page-banner" style="background-color: #c18d8f">
-    <!-- <div class="overlay"></div> -->
     <div class="page-banner-inner">
         <h1>Thanh toán</h1>
     </div>
 </div>
 
+
 <div class="page">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
+               
                 <?php if(!isset($_SESSION['customer'])): ?>
                     <p>
                         <a href="login.php" class="btn btn-md btn-danger">Vui lòng đăng nhập với tư cách khách hàng để tiến hành thanh toán.</a>
                     </p>
                 <?php else: ?>
+
 
                 <h3 class="special"><?php echo "Chi tiết đơn hàng" ?></h3>
                 <div class="cart">
@@ -49,103 +51,120 @@ if(!isset($_SESSION['cart_p_id'])) {
                             <th><?php echo "Số lượng"; ?></th>
                             <th class="text-right"><?php echo "Tổng cộng"; ?></th>
                         </tr>
-                         <?php
+                        <?php
                         $table_total_price = 0;
 
+
                         $i=0;
-                        foreach($_SESSION['cart_p_id'] as $key => $value) 
+                        foreach($_SESSION['cart_p_id'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_p_id[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_size_id'] as $key => $value) 
+                        foreach($_SESSION['cart_size_id'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_size_id[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_size_name'] as $key => $value) 
+                        foreach($_SESSION['cart_size_name'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_size_name[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_color_id'] as $key => $value) 
+                        foreach($_SESSION['cart_color_id'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_color_id[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_color_name'] as $key => $value) 
+                        foreach($_SESSION['cart_color_name'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_color_name[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_p_qty'] as $key => $value) 
+                        foreach($_SESSION['cart_p_qty'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_p_qty[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_p_current_price'] as $key => $value) 
+                        foreach($_SESSION['cart_p_current_price'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_p_current_price[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_p_name'] as $key => $value) 
+                        foreach($_SESSION['cart_p_name'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_p_name[$i] = $value;
                         }
 
+
                         $i=0;
-                        foreach($_SESSION['cart_p_featured_photo'] as $key => $value) 
+                        foreach($_SESSION['cart_p_featured_photo'] as $key => $value)
                         {
                             $i++;
                             $arr_cart_p_featured_photo[$i] = $value;
                         }
                         ?>
-                        <?php for($i=1;$i<=count($arr_cart_p_id);$i++): ?>
-                        <tr>
-                            <td><?php echo $i; ?></td>
-                            <td>
-                                <img src="assets/uploads/<?php echo $arr_cart_p_featured_photo[$i]; ?>" alt="">
-                            </td>
-                            <td><?php echo $arr_cart_p_name[$i]; ?></td>
-                            <td><?php echo $arr_cart_size_name[$i]; ?></td>
-                            <td><?php echo $arr_cart_color_name[$i]; ?></td>
-                            <td><?php echo $arr_cart_p_current_price[$i]; ?><?php echo "₫"; ?></td>
-                            <td><?php echo $arr_cart_p_qty[$i]; ?></td>
-                            <td class="text-right">
-                                <?php
-                                $row_total_price = intval($arr_cart_p_current_price[$i]) * intval($arr_cart_p_qty[$i]);
+                        <?php
+                        // Tính tổng đơn hàng
+                        $table_total_price = 0;
+                        for($i = 1; $i <= count($arr_cart_p_id); $i++) {
+                            $row_total_price = intval($arr_cart_p_current_price[$i]) * intval($arr_cart_p_qty[$i]);
+                            $table_total_price += $row_total_price;
+                        }
+                        ?>
 
-                                $table_total_price = $table_total_price + $row_total_price;
-                                ?>
-                                <?php echo $row_total_price; ?><?php echo "₫"; ?>
-                            </td>
-                        </tr>
-                        <?php endfor; ?>           
+
+                        <!-- Hiển thị chi tiết đơn hàng -->
+                        <?php
+                        for($i = 1; $i <= count($arr_cart_p_id); $i++) {
+                        ?>
+                            <tr>
+                                <td><?php echo $i; ?></td>
+                                <td><img src="assets/uploads/<?php echo $arr_cart_p_featured_photo[$i]; ?>" width="100" alt="<?php echo $arr_cart_p_name[$i]; ?>"></td>
+                                <td><?php echo $arr_cart_p_name[$i]; ?></td>
+                                <td><?php echo $arr_cart_size_name[$i]; ?></td>
+                                <td><?php echo $arr_cart_color_name[$i]; ?></td>
+                                <td><?php echo number_format($arr_cart_p_current_price[$i]); ?>₫</td>
+                                <td><?php echo $arr_cart_p_qty[$i]; ?></td>
+                                <td class="text-right"><?php echo number_format($arr_cart_p_current_price[$i] * $arr_cart_p_qty[$i]); ?>₫</td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                       
                         <tr>
                             <th colspan="7" class="total-text"><?php echo "Tạm tính"; ?></th>
-                            <th class="total-amount"><?php echo $table_total_price; ?><?php echo "₫"; ?></th>
+                            <th class="total-amount"><?php echo number_format($table_total_price); ?>₫</th>
                         </tr>
+
+
                         <?php
-                        // Lấy chi phí vận chuyển
-                        $statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost WHERE country_id=?");
-                        $statement->execute(array($_SESSION['customer']['cust_country']));
-                        $total = $statement->rowCount();
-                        if($total) {
+                        // LLấy phí vận chuyển phù hợp
+                        if ($table_total_price < 1500000) {
+                            $statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost WHERE country_id=?");
+                            $statement->execute(array($_SESSION['customer']['cust_country']));
                             $result = $statement->fetchAll(PDO::FETCH_ASSOC);
                             foreach ($result as $row) {
                                 $shipping_cost = $row['amount'];
@@ -157,22 +176,26 @@ if(!isset($_SESSION['cart_p_id'])) {
                             foreach ($result as $row) {
                                 $shipping_cost = $row['amount'];
                             }
-                        }                        
+                        }
                         ?>
+
+
                         <tr>
                             <td colspan="7" class="total-text"><?php echo "Phí vận chuyển"; ?></td>
-                            <td class="total-amount"><?php echo $shipping_cost; ?><?php echo "₫"; ?></td>
+                            <td class="total-amount"><?php echo number_format($shipping_cost); ?>₫</td>
                         </tr>
+
+
                         <tr>
                             <th colspan="7" class="total-text"><?php echo "Tổng cộng"; ?></th>
                             <th class="total-amount">
                                 <?php
-                                $final_total = $table_total_price+$shipping_cost;
+                                $final_total = $table_total_price + $shipping_cost;
                                 ?>
-                                <?php echo $final_total; ?><?php echo "₫"; ?>
+                                <?php echo number_format($final_total); ?>₫
                             </th>
                         </tr>
-                    </table> 
+                    </table>
                 </div>
 
                 <div class="billing-address">
