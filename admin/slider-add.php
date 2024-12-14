039cd0ce4d96dfda38b972ca8b1930d09d6c4ff7
viphan31221021781 <viphan.31221021781@st.ueh.edu.vn>
2024-12-14 22:51:18 +0700

    English

--- admin/slider-add.php
+++ admin/slider-add.php
@@ -110,9 +110,9 @@ if(isset($_POST['form1'])) {
 							<label for="" class="col-sm-2 control-label">Vị Trí </label>
 							<div class="col-sm-6">
 								<select name="position" class="form-control">
-									<option value="Trai">Trái</option>
-									<option value="Giua">Giữa</option>
-									<option value="Phai">Phải</option>
+									<option value="Left">Trái</option>
+									<option value="Center">Giữa</option>
+									<option value="Right">Phải</option>
 								</select>
 							</div>
 						</div>
